e9829363ff289b37d928a409dd8bd2408e1c2c0a
yuefan li <yuefanli@ctrip.com>
2020-11-09 10:09:31 +0800

    增加左侧公共弹出组件

---
+++ src/components/menu/drawer.tsx
@@ -0,0 +1,9 @@
+import './drawer.scss'
+import React from 'react';
+
+function Drawer(){
+    return (
+        <div className='component-menu-drawer'></div>
+    )
+}
+export default Drawer;
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,12 +1,28 @@
 import './index.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import { AppstoreAddOutlined, ExperimentOutlined, SettingOutlined, UserOutlined, HomeOutlined } from '@ant-design/icons';
 
 function Menu() {
+    const [panelState, SetPanelState] = useState({ name: 'home', show: false });
+
+    /**
+     * 点击composite-bar区域的icon
+     * 记录点击状态，并判断对应的功能面板展现与否
+     * @param iconName 
+     */
+    const clickCompositeIcon = function (iconName: string) {
+        if (panelState.name !== iconName) {
+            SetPanelState({ name: iconName, show: true })
+            return;
+        }
+
+        SetPanelState({ ...panelState, show: !panelState.show })
+    }
+
     return (
         <div className='component-menu'>
             <div className='composite-bar'>
-                <HomeOutlined className='menu-icon'></HomeOutlined>
+                <HomeOutlined className='menu-icon' onClick={() => clickCompositeIcon('homed')}></HomeOutlined>
                 <AppstoreAddOutlined className='menu-icon appstore' />
                 <ExperimentOutlined className='menu-icon'></ExperimentOutlined>
             </div>
